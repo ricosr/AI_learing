957cec6827a5b7dba60702d786b50959f1a6687b
rico <ricosr@163.com>
2018-04-27 20:08:14 +0800

    how to use matmul and plus in tensorflow

---
+++ tensorflow_prac/test_matmul_and_plus.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# test matmul and how to use + function
+
+
+import tensorflow as tf
+import numpy as np
+
+x_data = np.linspace(-1, 1, 5)[:, np.newaxis].astype(np.float32)
+Weights = tf.Variable(tf.random_normal([1, 3]), dtype=tf.float32)
+biases = tf.Variable(tf.zeros([1, 3]) + 0.1, dtype=tf.float32)
+
+Wx_plus_b = tf.matmul(x_data, Weights) + biases   # 每行都加一次
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# x = sess.run(x_data)
+print(x_data)
+print()
+
+w = sess.run(Weights)
+print(w)
+print()
+
+b = sess.run(biases)
+print(b)
+print()
+
+result = sess.run(Wx_plus_b)
+print(result)
+
