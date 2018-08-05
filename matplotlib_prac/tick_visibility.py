38aef8719e8b764d7aa689944189514b5e2124c1
ricosun <ricosr@163.com>
2018-08-05 17:16:44 +0800

    add tick visibility and text in matplotlib

---
+++ matplotlib_prac/tick_visibility.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-3, 3, 50)
+y = 0.001*x
+
+plt.figure()
+plt.plot(x, y, linewidth=10, zorder=1)    # set zorder for ordering the plot in plt 2.0.2 or higher
+plt.ylim(-2, 2)
+ax=plt.gca()
+ax.spines["right"].set_color("none")
+ax.spines["top"].set_color("none")
+ax.xaxis.set_ticks_position("bottom")
+ax.spines["bottom"].set_position(("data", 0))
+ax.yaxis.set_ticks_position("left")
+ax.spines["left"].set_position(("data", 0))
+
+# 调节透明度：
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontsize(12)
+    label.set_bbox(dict(facecolor="white", edgecolor="red", alpha=0.7, zorder=2))
+# set zorder for ordering the plot in plt 2.0.2 or higher
+# label.set_fontsize(12)重新调节字体大小, bbox设置目的内容的透明度相关参, facecolor调节box前景色, edgecolor设置边框, alpha设置透明度
+
+plt.show()
