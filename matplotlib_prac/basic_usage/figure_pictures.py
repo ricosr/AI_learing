a71e086b4e56265f3f1f28d2f30b8f2d2360dab3
ricosun <ricosr@163.com>
2018-08-07 18:43:05 +0800

    contours in matplotlib

--- matplotlib_prac/basic_usage/figure_pictures.py
+++ matplotlib_prac/basic_usage/figure_pictures.py
@@ -7,9 +7,9 @@ x1 = np.linspace(-3, 3, 50)
 y1 = 2*x1 + 1
 y2 = x1**2
 
-# plt.figure()
-# plt.plot(x1, y1)
-# plt.show()
+plt.figure()
+plt.plot(x1, y1)
+plt.show()
 
 plt.figure(num=3, figsize=(8, 5),)    # 编号为3；大小为(8, 5)
 plt.plot(x1, y2)
