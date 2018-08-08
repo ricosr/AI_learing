b14a3d732b148e8c00bfe453677087e4865ff7c4
ricosun <ricosr@163.com>
2018-08-08 16:56:09 +0800

    subplot in matplotlib

---
+++ matplotlib_prac/multiple_pictures/subplot.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 大小相同的图：
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.plot([0, 1], [0, 1])
+plt.subplot(2, 2, 2)
+plt.plot([0, 1], [0, 2])
+plt.subplot(223)
+plt.plot([0, 1], [0, 3])
+plt.subplot(224)
+plt.plot([0, 1], [0, 4])
+
+
+plt.figure(num=2)
+plt.subplot(2, 1, 1)
+plt.plot([0, 1], [0, 1])
+
+plt.subplot(2, 3, 4)    # 因为第一个subplot分了两行, 按照现在的分发, 第一次的第一行已经占了3个位置, 所以现在要选第4个
+plt.plot([0, 1], [0, 2])
+
+plt.subplot(235)
+plt.plot([0, 1], [0, 3])
+
+plt.subplot(236)
+plt.plot([0, 1], [0, 4])
+
+plt.show()
